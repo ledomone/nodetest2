2d21a945a8c1eb8537ff01b87d3461e3e441b043
ledomone <ledom.git@gmail.com>
2017-05-04 22:43:26 +0200

    Call Monk module

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest2');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
